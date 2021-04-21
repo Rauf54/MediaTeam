ea26ff00678f2b8c118f2790ba4c145f13987326
abdurrauf687 <abdurrauf687@gmail.com>
2021-04-21 10:48:45 +0500

    git added

---
+++ MediaTeam/Controllers/HomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeVC.swift
+//  MediaTeam
+//
+//  Created by Abdur Rauf on 21/04/2021.
+//  Copyright © 2021 Abdur Rauf. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
